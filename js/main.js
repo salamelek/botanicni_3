359e47dd93312573ec2ce90d4aea0f2cce752f75
salamelek <samuel.sluga@gmail.com>
2022-11-29 13:17:21 +0100

    finished gallery, make a good layout

--- js/main.js
+++ js/main.js
@@ -18,15 +18,15 @@ function toggleNav() {
     }
 }
 
-function toggleImgSize(url="") {
+function toggleImgSize(img_div=null) {
     let big_img_frame = document.getElementById("img-gallery-big");
     let big_img = document.getElementById("big-img");
 
     if (isImgBig) {
         big_img_frame.style = "transform: scale(0);";
         isImgBig = false;
-    } else {
-        big_img.src = url;
+    } else if (img_div !== null) {
+        big_img.src = img_div.children[0].src;
         big_img_frame.style = "transform: scale(100%);";
         isImgBig = true;
     }
