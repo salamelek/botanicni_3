f74345bbae36c88308a45badfa81e6dcc449b3ae
salamelek <samuel.sluga@gmail.com>
2023-01-26 15:57:17 +0100

    new database

--- database.php
+++ database.php
@@ -33,30 +33,82 @@ function create_db($conn): void {
     try {
         // the icon value will be a combinations of the icons (like the discord system)
         $conn->query("
-            CREATE TABLE IF NOT EXISTS Plants (
-            id int primary key auto_increment,
-            imeLat char(255) not null,
-            imeSlo char(255),
-            imeIta char(255),
-            drugaImenaSlo text,
-            sorta char(255),
-            druzina char(255),
-            izvor text,
-            habitat text,
-            opis text,
-            zanimivosti text,
-            isAtSchool bool,
-            iconsCode int not null,
-            unique key (imeLat)
-        );");
-
-        $conn->query("
-            CREATE TABLE IF NOT EXISTS Users (
-            id int primary key auto_increment,
-            username char(255) not null,
-            pswHash char(255) not null,
-            isAdmin bool not null
-        );");
+            create table if not exists Icons
+            (
+                id              int auto_increment
+                    primary key,
+                iconPath        char(255) not null,
+                iconDescription text      not null
+            );
+            
+            create table if not exists Plants
+            (
+                id            int auto_increment
+                    primary key,
+                imeLat        char(255)  not null,
+                imeSlo        char(255)  null,
+                imeIta        char(255)  null,
+                drugaImenaSlo text       null,
+                sorta         char(255)  not null,
+                druzina       char(255)  not null,
+                izvor         text       null,
+                habitat       text       null,
+                opis          text       null,
+                zanimivosti   text       null,
+                isAtSchool    tinyint(1) not null,
+                constraint imeLat
+                    unique (imeLat)
+            );
+            
+            create table if not exists `Plants-Icons`
+            (
+                plantId int not null,
+                iconId  int not null,
+                constraint plantIcons_pk
+                    unique (plantId, iconId),
+                constraint plantIcons_Icons_id_fk
+                    foreign key (plantId) references Icons (id)
+                        on update cascade on delete cascade,
+                constraint plantIcons_Plants_id_fk
+                    foreign key (plantId) references Plants (id)
+                        on update cascade on delete cascade
+            );
+            
+            create table if not exists Podrocja
+            (
+                id                  int auto_increment
+                    primary key,
+                namePodrocje        char(255) not null,
+                descriptionPodrocje text      null,
+                constraint namePodrocje
+                    unique (namePodrocje)
+            );
+            
+            create table if not exists `Podrocja-Plants`
+            (
+                podrocjeId int not null,
+                plantId    int not null,
+                constraint `Podrocja-Plants_pk`
+                    unique (podrocjeId, plantId),
+                constraint `Podrocja-Plants_Plants_id_fk`
+                    foreign key (plantId) references Plants (id)
+                        on update cascade on delete cascade,
+                constraint `Podrocja-Plants_Podrocja_id_fk`
+                    foreign key (podrocjeId) references Podrocja (id)
+                        on update cascade on delete cascade
+            );
+            
+            create table if not exists Users
+            (
+                id       int auto_increment
+                    primary key,
+                username char(255)  not null,
+                pswHash  char(255)  not null,
+                isAdmin  tinyint(1) not null,
+                constraint username
+                    unique (username)
+            );
+        ");
 
     } catch (PDOException $e) {
         echo "Error creating table: " . $e->getMessage();
