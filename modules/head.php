67b8f815a5f45cd0ba7145995dcbab23015bf5b9
salamelek <samuel.sluga@gmail.com>
2023-03-08 12:18:11 +0100

    i hate this

--- modules/head.php
+++ modules/head.php
@@ -5,6 +5,6 @@
   <meta charset="UTF-8">
   <meta name="author" content="salamelek">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel = "stylesheet" type = "text/css" href = "../css/style.css?v=1.0"/>
+  <link rel = "stylesheet" type = "text/css" href = "../css/style.css?v=1.1"/>
   <link rel="icon" href="../images/logo.png">
 </head>
